2b97e76d56148b10ac46085745a8367110c315f9
wcc19940308 <912328259@qq.com>
2019-05-29 21:59:41 +0800

    5-29

--- Exec.java
+++ Exec.java
@@ -1,5 +1,9 @@
 package LeetCode100;
 
-public class Exec {
+import java.util.concurrent.ConcurrentHashMap;
 
+public class Exec {
+    public static void main(String[] args) {
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+    }
 }
